23677f9722a17ae2b80d2826244ce5586235747c
iamclient <936095897@qq.com>
2026-01-31 02:28:11 +0800

    使用入口：添加方法2，支持项目本地管理logger，比如多个logger

--- init.go
+++ init.go
@@ -9,18 +9,27 @@ var (
 	superLogger  *zap.SugaredLogger
 )
 
-// InitLogger 初始化日志模块
-// logPath 若为空，则表示使用默认路径：./log/service.log
+// InitLogger 初始化日志模块。日志使用方法1：这里管理日志模块
+//
+//	logPath 若为空，则表示使用默认路径：./log/service.log
 func InitLogger(debugModel bool, logPath string) error {
+	fasterLogger, superLogger = NewLogger(debugModel, logPath)
+	return nil
+}
+
+// NewLogger 初始化日志模块。日志使用方法2：引用的项目自己管理日志模块
+//
+//	logPath 若为空，则表示使用默认路径：./log/service.log
+func NewLogger(debugModel bool, logPath string) (*zap.Logger, *zap.SugaredLogger) {
 	if logPath != "" {
 		logFilePath = logPath
 	}
 
 	core := newTree(debugModel)
 	options := newOption()
-	fasterLogger = zap.New(core, options...)
-	superLogger = fasterLogger.Sugar()
-	return nil
+	fLogger := zap.New(core, options...)
+	sLogger := fLogger.Sugar()
+	return fLogger, sLogger
 }
 
 // Sync 同步缓存
