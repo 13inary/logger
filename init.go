a9008c10af6898a2b2b3957ac20acf6f7637afe8
iamclient <936095897@qq.com>
2026-01-31 15:35:03 +0800

    NewLogger: 修复调用栈问题

--- init.go
+++ init.go
@@ -13,20 +13,25 @@ var (
 //
 //	logPath 若为空，则表示使用默认路径：./log/service.log
 func InitLogger(debugModel bool, logPath string) error {
-	fasterLogger, superLogger = NewLogger(debugModel, logPath)
+	fasterLogger, superLogger = newLogger(debugModel, logPath, 1)
 	return nil
 }
 
 // NewLogger 初始化日志模块。日志使用方法2：引用的项目自己管理日志模块
 //
 //	logPath 若为空，则表示使用默认路径：./log/service.log
+//	callerSkip 调用栈跳过层数
 func NewLogger(debugModel bool, logPath string) (*zap.Logger, *zap.SugaredLogger) {
+	return newLogger(debugModel, logPath, 0)
+}
+
+func newLogger(debugModel bool, logPath string, callerSkip int) (*zap.Logger, *zap.SugaredLogger) {
 	if logPath != "" {
 		logFilePath = logPath
 	}
 
 	core := newTree(debugModel)
-	options := newOption()
+	options := newOption(callerSkip)
 	fLogger := zap.New(core, options...)
 	sLogger := fLogger.Sugar()
 	return fLogger, sLogger
--- option.go
+++ option.go
@@ -6,10 +6,17 @@ import (
 )
 
 // newOption 额外的选项
-func newOption() []zap.Option {
-	return []zap.Option{
-		zap.AddCaller(), zap.AddCallerSkip(1), // 输出调用日志所在代码位置、跳过多少层调用栈
+func newOption(callerSkip int) []zap.Option {
+	options := []zap.Option{
+		zap.AddCaller(),
 		zap.AddStacktrace(zapcore.ErrorLevel), // 输出整个调用栈
 		//zap.Fields(zap.String("serverName", "api")), // 日志共有信息
 	}
+
+	// 输出调用日志所在代码位置、跳过多少层调用栈
+	if callerSkip > 0 {
+		options = append(options, zap.AddCallerSkip(callerSkip))
+	}
+
+	return options
 }
